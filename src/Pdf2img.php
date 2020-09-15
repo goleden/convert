41e756d7debc4d322cae50bd7ee766a6e0dce7bd
sorry <470134680@qq.com>
2020-09-15 16:59:32 +0800

    pdf2img转一张pdf

--- src/Pdf2img.php
+++ src/Pdf2img.php
@@ -23,7 +23,7 @@ class Pdf2img
      * @param string $compression 压缩
      * @param string $format 格式
      *
-     * @return string
+     * @return array
      */
     public function convert($pdf, $dir, $resoulution = '120', $compression = '100', $format = 'png')
     {
@@ -42,12 +42,47 @@ class Pdf2img
         }
         foreach ($im as $key => $var) {
             $var->setImageFormat($format);
-            $filename = rtrim($dir, '/'). '/'. uniqid(). rand(1000, 9999). '.'. $format;
+            $filename = rtrim($dir, '/') . '/' . uniqid() . rand(1000, 9999) . '.' . $format;
             if ($var->writeImage($filename) != true) {
                 throw new \Exception('Write image fail!');
             }
             $return[] = $filename;
         }
         return $return;
+    }
+
+    /**
+     * Pdf2img，转换一页pdf
+     *
+     * @param string $pdf pdf文件路径
+     * @param string $dir 保存目录
+     * @param string $page 页码
+     * @param string $resoulution 分辨率
+     * @param string $compression 压缩
+     * @param string $format 格式
+     *
+     * @return string
+     */
+    public function convertPage($pdf, $dir, $page = 0, $resoulution = '120', $compression = '100', $format = 'png')
+    {
+        if (!extension_loaded('Imagick')) {
+            throw new \Exception('Imagick extension not found!');
+        }
+        if (!is_file($pdf)) {
+            throw new \Exception('Pdf not found!');
+        }
+        $im = new \Imagick();
+        $im->setResolution($resoulution, $resoulution);
+        $im->setCompressionQuality($compression);
+        $im->readImage($pdf . '[' . $page . ']');
+        if (!is_dir($dir)) {
+            throw new \Exception('Dir not found!');
+        }
+        $im->setImageFormat($format);
+        $filename = rtrim($dir, '/') . '/' . uniqid() . rand(1000, 9999) . '.' . $format;
+        if ($im->writeImage($filename) != true) {
+            throw new \Exception('Write image fail!');
+        }
+        return $filename;
     }
 }
